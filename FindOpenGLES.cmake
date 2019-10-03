4b5d89e6628e1124544bcdc5a81709074755ef61
Alwin Esch <alwin.esch@web.de>
2019-10-03 17:13:28 +0200

    update FindOpenGLES.cmake to use also darwin_embedded

--- FindOpenGLES.cmake
+++ FindOpenGLES.cmake
@@ -1,24 +1,26 @@
 #.rst:
 # FindOpenGLES
 # ------------
-# Finds the OpenGLES2 library
+# Finds the OpenGLES2 and OpenGLES3 library
 #
-# This will define the following variables::
+# This will define the following variables:
 #
 # OPENGLES_FOUND - system has OpenGLES
 # OPENGLES_INCLUDE_DIRS - the OpenGLES include directory
 # OPENGLES_LIBRARIES - the OpenGLES libraries
 # OPENGLES_DEFINITIONS - the OpenGLES definitions
+#
 
 if(CORE_PLATFORM_NAME_LC STREQUAL rbpi)
-    set(_brcmprefix brcm)
+  set(_brcmprefix brcm)
 endif()
 
 if(PKG_CONFIG_FOUND)
   pkg_check_modules(PC_OPENGLES ${_brcmprefix}glesv2 QUIET)
 endif()
 
-if(NOT CORE_SYSTEM_NAME STREQUAL ios)
+if(NOT CORE_SYSTEM_NAME STREQUAL ios AND
+   NOT CORE_SYSTEM_NAME STREQUAL darwin_embedded)
   find_path(OPENGLES_INCLUDE_DIR GLES2/gl2.h
                                  PATHS ${PC_OPENGLES_INCLUDEDIR})
   find_library(OPENGLES_gl_LIBRARY NAMES ${_brcmprefix}GLESv2
